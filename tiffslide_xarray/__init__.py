c6b782ffa4d01fec9a12e386ad178873bd2681de
S. Joshua Swamidass <swamidass@gmail.com>
2023-11-06 10:15:23 -0600

    removed default import of accessors till api is better established

--- tiffslide_xarray/__init__.py
+++ tiffslide_xarray/__init__.py
@@ -4,9 +4,8 @@ https://github.com/swamidasslab/tiffslide-xarray
 """
 
 
-from . import accessors
 from .data import load_tiff_level, open_all_levels, TiffBackendEntrypoint
-
+import xarray as xr
 
 # Easier aliases
 open_level = load_tiff_level
